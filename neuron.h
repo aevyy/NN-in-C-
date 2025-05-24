78b23373a026cf97206322ab4dbb00c1b976dc6a
aevyy <wrk4avi@gmail.com>
2025-05-23 20:38:57 -0500

    adding the changes which was unsuccessful earlier

---
+++ neuron.h
@@ -0,0 +1,45 @@
+#ifndef _NEURON_H_
+#define _NEURON_H_4
+
+#include <iostream>
+
+class Neuron {
+public:
+
+    // Constructor
+    Neuron(double val);
+
+    /*
+    I am gopnna use a sigmoid function to actiuvate a neuyron
+    because it is easy to get it's derivative
+    f(x) = x / (1 + |x|)
+    */
+    void activate();
+
+    /*
+    derivative of fast sigmoid function
+    f'(x) = f(x) - (1 - f(x))
+    */
+    void derive();
+
+    //  Getters
+    double getVal() { return this->val; }
+    double getActivatedVal() { return this->activatedVal; }
+    double getDerivedVaL() { return this->derivedVal; }
+
+private:
+    // The current value of the neuron
+    double val;
+
+    // The activation value of the neuron
+    // This is also the squeezed value of the val
+    double activatedVal;
+
+    // The approximated deerivative of the direct value
+    double derivedVal;
+
+
+};
+
+
+#endif
