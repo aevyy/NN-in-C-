4005f9cdef6c819d5450efe2a9a1932774c38511
Avimanyu Ghimire <wrk4avi@gmail.com>
2025-05-25 23:15:57 +0000

    Fix: path to file typo fix

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,12 @@
 #include <iostream>
-#include "../header/Neuron.h"
+#include "../headers/Neuron.h"
+#include "../headers/Layer.h"
+#include "../headers/Matrix.h"
 
 using namespace std;
 
 int main() {
-    Neuron *n = new Neuron(0.8);
-    
-    // Initial tests
-    cout << "Value: " << n->getVal() << endl;
-    cout << "Activated Value: " << n->getActivatedVal() << endl;
-    cout << "Derived Value: " << n->getDerivedVal() << endl;
+
 
     return 0;
 }
