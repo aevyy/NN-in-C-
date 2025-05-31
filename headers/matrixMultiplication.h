50531c9296bf14c04d1a042c9e0986c491c14812
aevyy <wrk4avi@gmail.com>
2025-05-31 17:00:49 -0500

    Add: Header file for matrix multliplication utility class

---
+++ headers/matrixMultiplication.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <assert.h>
+
+#include "./matrix.h"
+
+namespace utils {
+    class multiplyMatrix {
+    public:
+        multiplyMatrix(Matrix *a, Matrix *b);
+        Matrix* execute();
+
+    private:
+        Matrix *a, *b, *c;
+    }
+}
