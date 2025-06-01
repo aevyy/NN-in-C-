849c5c30e6f6b9bd23a2451d52359d4aac2e80b0
aevyy <wrk4avi@gmail.com>
2025-06-01 00:29:45 -0500

    Implemented and defined some helper functions in NeuralNetwork header. Also defined and implemented feedForward in NeuralNetwork.

--- headers/NeuralNetwork.h
+++ headers/NeuralNetwork.h
@@ -6,6 +6,10 @@
 #include "Layer.h"
 #include "Matrix.h"
 
+#include "utils/MultiplyMatrix.h"
+#include "utils/MatrixToVector.h"
+
+
 class NeuralNetwork {
 public:
     // Constructor
@@ -16,6 +20,25 @@ public:
 
     // printing function
     void printToConsole();
+
+    void feedForward();
+    
+    // Funcitons to get neuron matrices
+    Matrix *getNeuronMatrix(int index) {
+        return this->layers.at(index)->matrixifyVals();
+    }
+
+    Matrix *getActivatedNeuronMatrix(int index) {
+        return this->layers.at(index)->matrixifyActivatedVals();
+    }
+
+    Matrix *getDerivedNeuronMatrix(int index) {
+        return this->layers.at(index)->matrixifyDerivedVals();
+    }
+
+    Matrix *getWeightMatrix(int index)  {
+        return this->weightMetrices.at(index);
+    }
 
 private:
     // Topology represents the number of elements (neurons) in each layer
--- src/NeuralNetwork.cpp
+++ src/NeuralNetwork.cpp
@@ -1,5 +1,23 @@
 #include "../headers/NeuralNetwork.h"
 
+void NeuralNetwork::feedForward() {
+    for (int i = 0; i < (this->layers.size() - 1); i++) {
+        Matrix *a = this->getNeuronMatrix(i);
+
+        if (i != 0) {
+            a = this->getActivatedNeuronMatrix(i);
+        }
+
+        Matrix *b = this->getWeightMatrix(i);
+        Matrix *c = (new utils::MultiplyMatrix(a, b))->execute();
+
+        std::vector<double> vals;
+        for (int c_index = 0; c_index < c->getNumCols(); c_index++) {
+            vals.push_back(c->getValue(0, c_index));
+        }
+    }
+}
+
 void NeuralNetwork::printToConsole() {
     for (int i = 0; i < this->layers.size(); i++) {
 
