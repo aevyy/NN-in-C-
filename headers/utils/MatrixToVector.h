44beb595a3b8fc12f1e8d20857bbde3d2654b6aa
aevyy <wrk4avi@gmail.com>
2025-05-31 21:50:09 -0500

    Add: Matrix to vector header file with definitions

---
+++ headers/utils/MatrixToVector.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <assert.h>
+
+#include "../Matrix.h"
+
+namespace utils {
+    class MatrixToVector {
+    public: 
+        MatrixToVector(Matrix *a);
+        std::vector<double> *execute();
+
+    private:
+        Matrix *a;
+    };
+}
