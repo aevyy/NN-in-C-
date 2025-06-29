3c9c8a75bb97cebee285ab4e9d1c550c6359c9b3
aevyy <wrk4avi@gmail.com>
2025-06-29 02:14:09 -0500

    modular main with support for command-line argument

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,35 @@
 #include <iostream>
-#include <fstream>
-#include <vector>
 #include <string>
-#include "../headers/json.hpp"
-#include "../headers/NeuralNetwork.h"
-#include "../headers/utils/DataLoader.h"
+#include "../headers/NetworkTrainer.h"
+#include "../headers/NetworkTester.h"
+#include "../headers/NetworkDemo.h"
 
-
-int main(int argc, char** argv) {
+int main(int argc, char* argv[]) {
+    try {
+        if (argc < 2) {
+            std::cout << ">> Neural Network System" << std::endl;
+            std::cout << "Usage: main [train|test|demo]" << std::endl;
+            return 0;
+        }
+        
+        std::string mode = argv[1];
+        
+        if (mode == "train") {
+            NetworkTrainer::trainWithConfig();
+        } else if (mode == "test") {
+            NetworkTester::testWithConfig();
+        } else if (mode == "demo") {
+            NetworkDemo::runDemoTests();
+        } else {
+            std::cout << "ERROR: Unknown mode: " << mode << std::endl;
+            std::cout << "Available modes: train, test, demo" << std::endl;
+            return 1;
+        }
+        
+    } catch (const std::exception& e) {
+        std::cerr << "ERROR: " << e.what() << std::endl;
+        return 1;
+    }
+    
     return 0;
 }
